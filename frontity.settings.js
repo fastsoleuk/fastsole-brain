659757e35f7584055f116f5cec827b3013b0382c
hardikaureate <hardik.devani@aureatelabs.com>
2022-04-08 12:06:19 +0530

    update config file

--- frontity.settings.js
+++ frontity.settings.js
@@ -2,7 +2,7 @@ const settings = {
   name: "fast-sole",
   state: {
     frontity: {
-      url: "https://brainfloss.in",
+      url: "https://fastsole.co.uk",
       title: "Test Frontity Blog",
       description: "WordPress installation for Frontity development",
       company_name: "FastSole",
@@ -32,7 +32,7 @@ const settings = {
       name: "@frontity/wp-source",
       state: {
         source: {
-          url: "https://brainfloss.in",
+          url: "https://fastsole.co.uk",
           postsPage: "/sneaker-news",
           categoryBase: "sneaker-news/category",
         },
